ca637ec0f2ecef38762172f0f3854780ed484bee
Yury Kavalenka <yurykavalenka@gmail.com>
2024-01-31 13:45:49 +0100

    add google maps

--- js/main.js
+++ js/main.js
@@ -7,3 +7,80 @@ navBtn.onclick = function () {
     nav.classList.toggle('header__top-row--mobile');
     document.body.classList.toggle('no-scroll');
 }
+
+
+
+/* Phone Mask */
+
+mask('[data-tel-input]');
+
+const phoneInputs = document.querySelectorAll('[data-tel-input]');
+
+phoneInputs.forEach((input) => {
+    input.addEventListener('input', ()=>{
+        if (input.value == '+') input.value = '';
+    })
+
+    input.addEventListener('blur', ()=>{
+        if (input.value == '+') input.value = '';
+    })
+});
+
+/* Google map */
+
+// ((g) => {
+//   var h,
+//     a,
+//     k,
+//     p = "The Google Maps JavaScript API",
+//     c = "google",
+//     l = "importLibrary",
+//     q = "__ib__",
+//     m = document,
+//     b = window;
+//   b = b[c] || (b[c] = {});
+//   var d = b.maps || (b.maps = {}),
+//     r = new Set(),
+//     e = new URLSearchParams(),
+//     u = () =>
+//       h ||
+//       (h = new Promise(async (f, n) => {
+//         await (a = m.createElement("script"));
+//         e.set("libraries", [...r] + "");
+//         for (k in g)
+//           e.set(
+//             k.replace(/[A-Z]/g, (t) => "_" + t[0].toLowerCase()),
+//             g[k]
+//           );
+//         e.set("callback", c + ".maps." + q);
+//         a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
+//         d[q] = f;
+//         a.onerror = () => (h = n(Error(p + " could not load.")));
+//         a.nonce = m.querySelector("script[nonce]")?.nonce || "";
+//         m.head.append(a);
+//       }));
+//   d[l]
+//     ? console.warn(p + " only loads once. Ignoring:", g)
+//     : (d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)));
+// })({
+//   key: "AIzaSyC9vMMomm79tBDMh7kxn6ENQmIpSjFMvE8",
+//   v: "weekly",
+//   // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+//   // Add other bootstrap parameters as needed, using camel case.
+// });
+
+
+
+// let map;
+
+// async function initMap() {
+//   //@ts-ignore
+//   const { Map } = await google.maps.importLibrary("maps");
+
+//   map = new Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+// }
+
+// initMap();
